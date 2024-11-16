29f96cda2465272b438822f530bdad5acc2c14a4
Valudiaz78 <167375320+Valudiaz78@users.noreply.github.com>
2024-11-16 20:54:00 -0300

    Update main.go

--- tp2/main.go
+++ tp2/main.go
@@ -6,27 +6,43 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"tdas/heap"
+	"tdas/lista"
 	"time"
 )
 
+type lineaLog struct {
+	ip    string
+	fecha time.Time
+	URL   string
+}
+
 func main() {
-	comando_ingresado := os.Args[0]
+	comando_ingresad := os.Args[0]
 	if comando_ingresado == "agregar_archivo" {
 		ruta_log := os.Args[1]
 		agregar_archivo(ruta_log)
-	} elif comando_ingresado == "ver_visitantes" {
+	} else if comando_ingresado == "ver_visitantes" {
 		desde := os.Args[1]
 		hasta := os.Args[2]
 		ver_visitantes(desde, hasta)
-	} elif comando_ingresado == "mas_visitados" {
+	} else if comando_ingresado == "mas_visitados" {
 		n := os.Args[1]
 		mas_visitados(n)
 	}
-	
 }
 
-func agregar_archivo(entrada string) {
-
+func agregar_archivo(ruta_archivo string) {
+	archivo, err := os.Open(ruta_archivo)
+	defer archivo.Close()
+	var lineas []lineaLog
+	s := bufio.NewScanner(archivo)
+	for s.Scan() {
+		linea := s.Text()
+		nueva_entrada := procesar_linea(linea) // Convierte a la linea en el struct
+		lineas = append(lineas, nueva_entrada)
+	}
+	busquedaDOS(lineas)
 }
 
 func ver_visitantes(desde string, hasta string) {
@@ -35,4 +51,23 @@ func ver_visitantes(desde string, hasta string) {
 
 func mas_visitados(n int) {
 
+}
+
+// Aux
+
+func procesar_linea(linea string) lineaLog {
+	palabras := strings.Fields(linea)
+	ip := palabras[0]
+	fecha := palabras[1]
+	URL := palabras[3]
+	fecha_parseada, _ := time.Parse("2006-01-02T15:04:05", fecha)
+	linea_log := lineaLog{ip, fecha_parseada, URL}
+	return linea_log
+}
+
+func busquedaDOS(lineas []lineaLog) {
+
+	for _, linea := range lineas {
+
+	}
 }
